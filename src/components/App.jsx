bd01be494e46e42244a7e83c871f1cd7ca7dcfb3
docnexus <drlamb1@gmail.com>
2020-06-05 22:13:53 -0500

    changed items to array, used components and props to dynamically populate list items using map, saved the value being entered using hooks, added the new value to the end of the existing array using the spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,41 @@
 import React from "react";
+import List from "./List";
 
 function App() {
+  const [items, setItems] = React.useState(["A Item"]);
+  const [enteredItem, setEnteredItem] = React.useState("");
+
+  function handleInput(event) {
+    setEnteredItem(event.target.value);
+  }
+
+  function handleSubmit() {
+    setItems((items => ([...items, [enteredItem]])))
+  }
+  
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          type="text"
+          placeholder="Enter a new item"
+          value={enteredItem}
+          onChange={handleInput}
+        />
+        <button onClick={handleSubmit}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((items) => (
+            <List items={items} />
+          ))}
+          ;
         </ul>
       </div>
     </div>
